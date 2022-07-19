3e397a06756d5b2c53c9134575a6a9d2a4aa6ca3
fernandorosa <stevensfrr@gmail.com>
2022-07-18 22:24:53 -0400

    Initial commit

---
+++ src/result.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class result {
+    int hold = 0;
+
+    List<Integer> history = new ArrayList<>();
+
+    public static int count(int n, int k){
+        int res = 0;
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
